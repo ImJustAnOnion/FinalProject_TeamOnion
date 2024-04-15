d45e1f4b92b32432f40f5f193eff098aa9fddc9e
Lai Connelly <laicoal@hotmail.com>
2024-04-15 15:16:40 -0400

    added game links to homepage and styled them

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,14 @@
+import { Link } from "react-router-dom";
+
 const HomePage = () => {
     return (
         <div>
             <h1>This is the Home Page</h1>
             <p>Disclaimer: By playing any games on this site you are agreeing to sell your soul.</p>
+            <ul id="HomepageNav">
+                <li><Link to="/rps">Rock Paper Scissors</Link></li>
+                <li><Link to="/tictactoe">Tic Tac Toe</Link></li>
+            </ul>
         </div>
     );
 };
