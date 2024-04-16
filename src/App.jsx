73ac6a7b12a631cccd9d30c81ee8588d4059caff
atkinsec <atkinsec@mail.uc.edu>
2024-04-16 02:57:47 -0400

    redo!!

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,22 @@
-import MenuBar  from './components/MenuBar'
-import Game from './components/Game'
-import { Wordle } from './components/wordle'
-import { useState, useEffect, useMemo } from 'react'
-import './App.css'
+import Grid from './components/Grid'; 
+import Button from './components/Button';
+import './App.css';
 
-function App() {
-  const [loading, setLoading] = useState(true);
-  const wordle = useMemo(() => new Wordle(), []);
 
-  useEffect(() => {
-    wordle.setupGame().then(() => setLoading(false));
-    const handleKeyDown = async (event) => {
-      wordle.keydownHandler(event.key);
-    };
-    window.addEventListener('keydown', handleKeyDown);
-    return () => window.removeEventListener('keydown', handleKeyDown);
-  }, [wordle])
+export default function App() {
 
-  if (loading) {
-    return <div>Loading...</div>
-  }
 
   return (
-    <div id="app" className='flex h-screen w-screen flex-col items-center justify-center'>
-      <MenuBar />
-      <Game word={wordle.gameConfig.word} guess={wordle.gameState.currentGuess} attempt={wordle.gameState.currentAttempt}/>
+    <div id="app" className='w-screen'>
+      <header>
+          <h1 className='uppercase font-bold tracking-widest'>Wordle</h1>
+          <div id="option-bar">
+              <Button />
+          </div>
+      </header>
+      <div id="game">
+        <Grid />
+      </div>
     </div>
-  )
+  );
 }
-
-export default App;
